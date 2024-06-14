937bc8b12b9ef63c0c748979283d49a55bb02bc3
farhancirrus <farhan.soomro@cirruslabs.io>
2024-06-14 16:11:14 -0400

    test commit to see expo app is working and github working, changed welcome msg to welcome its farhan

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Welcome! its farhan</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
